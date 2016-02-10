e0b4e0d377109ab6df3e8e6b0dcb30ecdd0d524c
Mislav Javor <mislav.javor@outlook.com>
2016-02-11 00:47:28 +0100

    Initial commit

---
+++ app/src/main/java/com/mislavjavor/belablok/database/DatabaseInstance.java
@@ -0,0 +1,13 @@
+package com.mislavjavor.belablok.database;
+
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.Database;
+
+/**
+ * Created by mislavjavor on 11/02/16.
+ */
+public class DatabaseInstance {
+    public static Database getDatabaseInstance() throws CouchbaseLiteException{
+
+    }
+}
